4a494026bd26e415cc7b32ce452b191b2e15543c
John Choi <jchoi950@Johns-MacBook-Pro.local>
2020-02-11 00:04:29 -0800

    populated weather based on loccation information

--- components/app.js
+++ components/app.js
@@ -1,23 +1,48 @@
 class App {
-    constructor () {
+    constructor (quotes) {
         this.getLocationSuccess = this.getLocationSuccess.bind(this);
         this.getLocationError = this.getLocationError.bind(this);
+        this.getWeatherSuccess = this.getWeatherSuccess.bind(this);
+        this.getWeatherError = this.getWeatherError.bind(this);
+        this.quotes = quotes;
+        this.userLocation = null;
+        this.currentWeather = null;
     };
     getLocation () {
         $.ajax ({
-            method: "GET",
             url: "http://ip-api.com/json/",
             success: this.getLocationSuccess,
             error: this.getLocationError
         });
     };
     getLocationSuccess (data) {
-        console.log(data);
+        this.userLocation = data;
+        this.getWeather(this.userLocation.city);
     };
-    getLocationError () {
+    getLocationError (error) {
+        console.log(error);
+    };
+    getWeather (userLocation) {
+        $.ajax ({
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + userLocation + "&appid=0b810be14937d73254c214a19e48465c",
+            success: this.getWeatherSuccess,
+            error: this.getWeatherError
+        });
+    };
+    getWeatherSuccess (data) {
+        this.currentWeather = data;
+        var city = this.userLocation.city;
+        var state = this.userLocation.region;
+        var temperature = this.currentWeather.main.temp;
+        var weatherIcon = this.currentWeather.weather[0].icon;
+        document.querySelector("#location-display").textContent = city + ", " + state;
+        document.querySelector("#temperature-display").innerHTML = ((Number(temperature) - 273.15) * 9/5 + 32).toFixed(2) + "<span>&#8457;<span> / " + ((Number(temperature) - 273.15)).toFixed(2) + "<span>&#8451;<span>";
+        document.querySelector("#weather-icon").innerHTML = "<img src=http://openweathermap.org/img/wn/" + weatherIcon + ".png>";
+    };
+    getWeatherError (error) {
         console.log(error);
     };
     start () {
         this.getLocation();
-    }
- }
+    };
+ };
