38ecd84c4af02cea6b039221cf24cdd8d920efca
Julian Tuazon <jthrow00@gmail.com>
2020-02-11 19:59:51 -0800

    Created Photo class with constructor and corresponding functions

---
+++ photo.js
@@ -0,0 +1,69 @@
+class Photos {
+  constructor() {
+    this.handleGetPhotoSuccess = this.handleGetPhotoSuccess.bind(this);
+    this.handleGetPhotoError = this.handleGetPhotoError.bind(this);
+    this.initializeModal = this.initializeModal.bind(this);
+    this.getPhotos = this.getPhotos.bind(this);
+  }
+
+  handleGetPhotoSuccess(data) {
+    console.log(data);
+
+    var counter = 0;
+    while (counter < 4) {
+      var randomIndex = Math.floor(Math.random() * (data.photos.length));
+      if (!selectedPhotos.includes(data.photos[randomIndex])) {
+        selectedPhotos.push(data.photos[randomIndex]);
+        counter++;
+      }
+    }
+    console.log("final selected photos", selectedPhotos);
+
+    $(".d-block").each(function (i) {
+      this.src = selectedPhotos[i].src.large;
+      console.log("this.source", this.src);
+    })
+}
+
+    // console.log(`"${data.quoteText}" - ${data.quoteAuthor}`);
+    // var quoteContent1 = document.querySelector('.quote-text');
+    // var quoteContent2 = document.querySelector('.quote-author');
+    // var qButton = document.querySelector('#quote-button');
+
+    // quoteContent1.textContent = `"${data.quoteText}"`;
+    // quoteContent2.textContent = `- ${data.quoteAuthor}`;
+
+    // if (data.quoteAuthor.length === 0) {
+    //   quoteContent2.textContent = "Unknown"
+    // }
+    // else {
+    //   quoteContent2.textContent = `${data.quoteAuthor}`;
+    // }
+
+  //   qButton.addEventListener("click", this.initializeModal)
+  // }
+
+
+  handleGetPhotoError(error) {
+    console.log(error);
+  }
+
+  initializeModal() {
+    console.log(" photo button clicked");
+    const homePage = document.getElementById('home-page');
+    const photoModal = document.getElementById('photo-modal');
+    photoModal.classList.remove('hidden');
+    homePage.classList.add('hidden');
+    this.getPhotos;
+  }
+
+  getPhotos() {
+    $.ajax({
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader("Authorization", api_key);
+      },
+      url: "https://api.pexels.com/v1/search?query=" + search + "&per_page=80&page=1",
+      success: this.handleGetPhotoSuccess,
+      error: this.handleGetPhotoError,
+    )}
+}
